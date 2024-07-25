f346d11c625b80a120ed69f8e48924087fd277b6
Edgar0223 <edgarbarajas@MacBook-Air-de-Edgar.local>
2024-07-25 14:38:19 -0600

    nueva

--- Ali/settings.py
+++ Ali/settings.py
@@ -167,7 +167,7 @@ STATICFILES_DIRS = [
     "Aplicaciones/cuentas/static",
     "Aplicaciones/Calidad/static", 
     "Aplicaciones/Produccion/static",
-    "Aplicaciones/Recepcion/static"
+    "Aplicaciones/Recepcion/static",
     ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
