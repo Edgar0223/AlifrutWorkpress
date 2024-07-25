a4a99c4956225658242427ae58f8a3b50c63d48d
Edgar0223 <edgarbarajas@MacBook-Air-de-Edgar.local>
2024-07-25 11:35:58 -0600

    Mensaje de commit

--- Ali/settings.py
+++ Ali/settings.py
@@ -162,7 +162,13 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ 'Aplicaciones/Trabajo/static',BASE_DIR / "Aplicaciones/Trabajo/static" / "Aplicaciones/Calidad/static" / "Aplicaciones/Produccion/static" / "Aplicaciones/Recepcion/static"]
+STATICFILES_DIRS = [ 
+    'Aplicaciones/Trabajo/static',
+    "Aplicaciones/Trabajo/static",
+    "Aplicaciones/Calidad/static", 
+    "Aplicaciones/Produccion/static",
+    "Aplicaciones/Recepcion/static"
+    ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
